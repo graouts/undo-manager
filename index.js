6c9dd4f238c53286f33147911995dc74229fa599
Antoine Quint <antoine@graougraou.com>
2014-05-19 10:34:50 +0200

    Add support for grouping.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function UndoManager()
     this._index = 0;
     this._actions = [];
 
+    this._groupedActions = [];
+    this._groupingLevel = 0;
+
     this.isUndoing = false;
     this.isRedoing = false;
 }
@@ -36,7 +39,7 @@ UndoManager.prototype = {
         this._index--;
 
         this.isUndoing = true;
-        this._actions[this._index]();
+        this._executeAction();
         this.isUndoing = false;
     },
 
@@ -48,8 +51,32 @@ UndoManager.prototype = {
         this._index++;
 
         this.isRedoing = true;
-        this._actions[this._index]();
+        this._executeAction();
         this.isRedoing = false;
+
+        if (this._index === this._actions.length - 1) {
+            this._actions.pop();
+            this._index--;
+        }
+    },
+
+    beginGroup: function()
+    {
+        this._groupingLevel++;
+    },
+
+    endGroup: function()
+    {
+        if (!this._groupingLevel)
+            return;
+        
+        this._groupingLevel--;
+
+        if (this._groupingLevel > 0)
+            return;
+
+        this._registerAction(this._groupedActions);
+        this._groupedActions = [];
     },
 
     registerPropertyUndo: function(target, propertyName, value)
@@ -57,15 +84,18 @@ UndoManager.prototype = {
         if (this.shouldIgnoreRegisterCalls)
             return;
 
-        this.registerFunctionUndo(function() {
+        var fun = function() {
             target[propertyName] = value;
-        });
+        };
+        fun.value = value;
+        this.registerFunctionUndo(fun);
     },
 
     registerMethodUndo: function(target, method)
     {
         if (this.shouldIgnoreRegisterCalls)
             return;
+
         var args = Array.prototype.slice.call(arguments, 2);
         this.registerFunctionUndo(function() {
             method.apply(target, args);
@@ -77,12 +107,31 @@ UndoManager.prototype = {
         if (this.shouldIgnoreRegisterCalls)
             return;
 
+        if (this._groupingLevel > 0)
+            this._groupedActions.push(fun);
+        else
+            this._registerAction(fun);
+    },
+
+    _registerAction: function(action)
+    {
         if (this.canRedo)
             this._actions.splice(this._index, Number.MAX_VALUE);
 
-        this._index = this._actions.push(fun);
+        this._index = this._actions.push(action);
         if (this.isUndoing)
             this._index -= 2;
-    }
+    },
 
+    _executeAction: function()
+    {
+        var action = this._actions[this._index];
+        if (typeof action === "function")
+            action();
+        else if (Array.isArray(action)) {
+            (this.isUndoing ? action.slice().reverse(): action).forEach(function(groupedAction) {
+                groupedAction();
+            });
+        }
+    }
 };
